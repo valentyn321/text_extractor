4ae5c7608a1358e6fe1f83c4cb5db7e74e888af1
Valentyn <valentyncherkasov24@gmail.com>
2022-03-29 14:57:07 +0300

    removes unuseful endpoints and unittests, creates testing endpoint and unittets for it

--- app/main.py
+++ app/main.py
@@ -1,19 +1,46 @@
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import io
+import uuid
+from functools import lru_cache
+from fastapi import Depends, FastAPI, File, HTTPException, Request, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from PIL import Image
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploads"
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
-@app.get("/", response_class=HTMLResponse)
-def home_view(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
-
-
-@app.post("/")
-def home_detail_view():
-    return {"hello": "world!"}
+@app.post("/img-echo/", response_class=FileResponse)
+async def img_echo_view(
+    file: UploadFile = File(...), settings: Settings = Depends(get_settings)
+):
+    if settings.echo_active:
+        UPLOAD_DIR.mkdir(exist_ok=True)
+        bytes_str = io.BytesIO(await file.read())
+        try:
+            image = Image.open(bytes_str)
+        except:
+            raise HTTPException(detail="Invalid file format", status_code=400)
+        fname = pathlib.Path(file.filename)
+        dest = UPLOAD_DIR / f"{uuid.uuid1()}{fname.suffix}"
+        image.save(dest)
+        return dest
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,18 +1,36 @@
-from http import client
+import io
+import shutil
+import time
+from click import echo
+
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import UPLOAD_DIR, app, BASE_DIR
+from PIL import Image, ImageChops
 
 client = TestClient(app)
 
 
-def test_get_home():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert "text/html" in response.headers["content-type"]
+def test_img_echo():
+    path = BASE_DIR / "images"
+    for img in path.glob("*"):
+        try:
+            image = Image.open(img)
+        except:
+            image = None
 
+        try:
+            response = client.post("/img-echo/", files={"file": open(img, "rb")})
+            if image is None:
+                assert response.status_code == 400
+            else:
+                # Returning a valid image
+                assert response.status_code == 200
+                r_stream = io.BytesIO(response.content)
+                echo_img = Image.open(r_stream)
+                difference = ImageChops.difference(echo_img, image).getbbox()
+                assert difference in None
+        except TypeError:
+            pass
 
-def test_post_home():
-    response = client.post("/")
-    assert response.status_code == 200
-    assert "application/json" in response.headers["content-type"]
-    assert response.json() == {"hello": "world!"}
+    time.sleep(3)
+    shutil.rmtree(UPLOAD_DIR)
