3caf5c3c28f0f339b7b8b6a4f38efd6a19e43cc3
Valentyn <valentyncherkasov24@gmail.com>
2022-03-29 12:07:04 +0300

    configuring the environment, pytest, pre-commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.post("/")
+def home_detail_view():
+    return {"hello": "world!"}
