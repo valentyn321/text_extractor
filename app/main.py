f5eac732151ec989b8f2aea094456b9453ff109f
Valentyn <valentyncherkasov24@gmail.com>
2022-03-29 15:56:58 +0300

    adds token authorization

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import uuid
 import pytesseract
 
 from functools import lru_cache
-from fastapi import Depends, FastAPI, File, HTTPException, Request, UploadFile
+from fastapi import Depends, FastAPI, File, Header, HTTPException, Request, UploadFile
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
@@ -17,6 +17,9 @@ UPLOAD_DIR = BASE_DIR / "uploads"
 class Settings(BaseSettings):
     debug: bool = False
     echo_active: bool = False
+    app_auth_token: str
+    app_auth_token_prod: str = None
+    skip_auth: bool = False
 
     class Config:
         env_file = ".env"
@@ -31,10 +34,26 @@ app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
+def verify_auth(
+    authorization=Header(None),
+    settings: Settings = Depends(get_settings),
+):
+    if settings.debug and settings.skip_auth:
+        return
+    if authorization is None:
+        raise HTTPException(detail="Invalid authorization", status_code=401)
+    label, token = authorization.split()
+    if token != settings.app_auth_token:
+        raise HTTPException(detail="Invalid authorization", status_code=401)
+
+
 @app.post("/")
 async def prediction_view(
-    file: UploadFile = File(...), settings: Settings = Depends(get_settings)
+    file: UploadFile = File(...),
+    authorization=Header(None),
+    settings: Settings = Depends(get_settings),
 ):
+    verify_auth(authorization, settings)
     bytes_str = io.BytesIO(await file.read())
     try:
         img = Image.open(bytes_str)
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -3,7 +3,7 @@ import shutil
 import time
 
 from fastapi.testclient import TestClient
-from app.main import UPLOAD_DIR, app, BASE_DIR
+from app.main import UPLOAD_DIR, app, BASE_DIR, get_settings
 from PIL import Image, ImageChops
 
 client = TestClient(app)
@@ -11,6 +11,7 @@ client = TestClient(app)
 
 def test_prediction():
     path = BASE_DIR / "images"
+    settings = get_settings()
     for img in path.glob("*"):
         try:
             image = Image.open(img)
@@ -18,12 +19,29 @@ def test_prediction():
             image = None
 
         try:
-            response = client.post("/", files={"file": open(img, "rb")})
+            response = client.post(
+                "/",
+                files={"file": open(img, "rb")},
+                headers={"Authorization": f"JWT {settings.app_auth_token}"},
+            )
             if image is None:
                 assert response.status_code == 400
             else:
                 assert response.status_code == 200
                 assert len(response.json()) == 2
+        except TypeError:
+            pass
+
+
+def test_prediction_fails_because_of_missing_headers():
+    path = BASE_DIR / "images"
+    for img in path.glob("*"):
+        try:
+            response = client.post(
+                "/",
+                files={"file": open(img, "rb")},
+            )
+            assert response.status_code == 401
         except TypeError:
             pass
 
